35df6ed1fd441bb5dc719bf6470d7c028c2d9ef3
JensonMedina <medinajenson74@gmail.com>
2024-07-11 19:22:35 -0300

    Reorganicé los archivos

--- BullCoin/BullCoin/js/pages/register.js
+++
@@ -1,8 +0,0 @@
-﻿function successfullyRegistered() {
-    Swal.fire({
-        title: "¡Cuenta creada!",
-        text: "Cuenta creada correctamente.",
-        icon: "success",
-        confirmButtonText: "OK"
-    });
-}
